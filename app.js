bdfa0567f16519c090ca7c12bb9aaa45ba19d007
Zimo Dong <andy199354@gmail.com>
2018-03-15 09:53:19 -0700

    added Geocode Api with ajax

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-// function initMap() {
-//   var uluru = {lat: -25.363, lng: 131.044};
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 4,
-//     center: uluru
-//   });
-//   var marker = new google.maps.Marker({
-//     position: uluru,
-//     map: map
-//   });
-// }
 
+//initialize a map
 function initMap() {
+  let current_pos = '';
+  let current_coord = {};
+
   const Vancouver = {lat: 49.246 , lng: -123.116};
-  const map = new google.maps.Map(document.getElementById('map'), {
+
+  const options = {
     zoom: 8,
     center: Vancouver
-  });
-  const marker = new google.maps.Marker({
+  };
+
+  const map = new google.maps.Map(document.getElementById('map'),options );
+  //initial marker
+  const initial_marker = new google.maps.Marker({
     position: Vancouver,
     map: map
   });
+
+  //Geocode API with AJAX
+  const getLocationData = (addr) =>{
+     let GeoAPI = "https://maps.googleapis.com/maps/api/geocode/json?address="+ addr +"&key=AIzaSyBNa4rB5d9Oss2Ff2L6daE99fG4Ue4OvIQ";
+     const showResult = (data)=>{
+       console.log(data);
+       current_coord = data.results[0].geometry.location;
+       console.log(current_coord);
+     };
+     //AJAX
+     $.getJSON(GeoAPI,showResult);
+
+  };
+  //event handlers
+  document.getElementById("button").addEventListener("click", ()=>{
+      current_pos = document.getElementById("addr").value;
+      console.log(current_pos);
+      getLocationData(current_pos);
+
+  });
+  document.getElementById("addr").addEventListener("keyup", (e)=>{
+     if(e.keyCode === 13){
+        document.getElementById("button").click();
+     }
+  });
+
 }
